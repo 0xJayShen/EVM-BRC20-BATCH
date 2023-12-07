744d562877033db3e56b9c40841d441b3ec20eed
jayshen <976739120@qq.com>
2023-12-07 13:45:06 +0800

    commit

---
+++ go.mod
@@ -0,0 +1,24 @@
+module evm-brc20-batch
+
+go 1.18
+
+require github.com/ethereum/go-ethereum v1.10.23
+
+require (
+	github.com/StackExchange/wmi v1.2.1 // indirect
+	github.com/btcsuite/btcd/btcec/v2 v2.2.0 // indirect
+	github.com/deckarep/golang-set v1.8.0 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
+	github.com/go-ole/go-ole v1.2.5 // indirect
+	github.com/go-stack/stack v1.8.1 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/holiman/uint256 v1.2.3 // indirect
+	github.com/shirou/gopsutil v3.21.4-0.20210419000835-c7a38de76ee5+incompatible // indirect
+	github.com/stretchr/testify v1.8.2 // indirect
+	github.com/tklauser/go-sysconf v0.3.12 // indirect
+	github.com/tklauser/numcpus v0.6.1 // indirect
+	golang.org/x/crypto v0.14.0 // indirect
+	golang.org/x/sync v0.3.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	gopkg.in/natefinch/npipe.v2 v2.0.0-20160621034901-c1b8fa8bdcce // indirect
+)
---
+++ main.go
@@ -0,0 +1,140 @@
+package main
+
+import (
+	"context"
+	"crypto/ecdsa"
+	"fmt"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"log"
+	"math/big"
+	"os"
+	"strconv"
+	"time"
+)
+
+func getNonce(node, priv string) (uint64, error) {
+	privateKey, err := crypto.HexToECDSA(priv)
+	if err != nil {
+		log.Fatalf("Failed to convert private key: %v", err)
+		return 0, err
+	}
+
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		log.Fatal("Error casting public key to ECDSA")
+		return 0, err
+	}
+
+	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+	client, err := ethclient.Dial(node)
+	if err != nil {
+		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
+		return 0, err
+	}
+	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+	if err != nil {
+		log.Fatalf("Failed to get nonce: %v", err)
+		return 0, err
+	}
+	fmt.Println("nonce is ------- ", nonce)
+	return nonce, nil
+}
+func getChainID(node string) (*big.Int, error) {
+	client, err := ethclient.Dial(node)
+	if err != nil {
+		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
+		return nil, err
+	}
+	chainID, err := client.NetworkID(context.Background())
+	if err != nil {
+		log.Fatalf("Failed to get chain ID: %v", err)
+		return nil, err
+	}
+	fmt.Println("chainID ------- ", chainID)
+	return chainID, nil
+}
+func do(node string, priv string, msg string, nonce uint64, chainID *big.Int, gasPrice_ int64) error {
+	client, err := ethclient.Dial(node)
+	if err != nil {
+		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
+		return err
+	}
+
+	privateKey, err := crypto.HexToECDSA(priv)
+	if err != nil {
+		log.Fatalf("Failed to convert private key: %v", err)
+		return err
+	}
+
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		log.Fatal("Error casting public key to ECDSA")
+	}
+
+	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+
+	value := big.NewInt(0)
+	gasLimit := uint64(22000)
+	gasPrice := big.NewInt(gasPrice_)
+	if err != nil {
+		log.Fatalf("Failed to suggest gas price: %v", err)
+		return err
+	}
+	data := []byte(msg)
+	tx := types.NewTransaction(nonce, fromAddress, value, gasLimit, gasPrice, data)
+	signedTx, err := types.SignTx(tx, types.NewEIP155Signer(chainID), privateKey)
+	if err != nil {
+		log.Fatalf("Failed to sign transaction: %v", err)
+		return err
+	}
+	err = client.SendTransaction(context.Background(), signedTx)
+	if err != nil {
+		log.Fatalf("Failed to send transaction: %v", err)
+		return err
+	}
+
+	fmt.Printf("Transaction sent! TX Hash: %s\n", signedTx.Hash().Hex())
+	return nil
+}
+func main() {
+	var (
+		Node      = os.Args[1]
+		Priv      = os.Args[2]
+		Msg       = os.Args[3]
+		Gas       = os.Args[4]
+		LoopTotal = os.Args[5]
+		Sleep     = os.Args[6]
+	)
+
+	nonceNow, err := getNonce(Node, Priv)
+	if err != nil {
+		panic(err)
+	}
+	chainID, err := getChainID(Node)
+	if err != nil {
+		panic(err)
+	}
+	gas, err := strconv.ParseInt(Gas, 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	loopTotal_, err := strconv.ParseInt(LoopTotal, 10, 64)
+	if err != nil {
+		panic(err)
+	}
+
+	sleel_, err := strconv.ParseInt(Sleep, 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	for i := 0; i < int(loopTotal_); i++ {
+		time.Sleep(time.Duration(sleel_) * time.Millisecond)
+		if err := do(Node, Priv, Msg, nonceNow, chainID, gas); err != nil {
+			panic(err)
+		}
+	}
+}
