5216de90e196271ff8ea82cf19791f02b86173d7
jayshen <976739120@qq.com>
2023-12-08 14:23:08 +0800

    commit

--- main.go
+++ main.go
@@ -4,141 +4,204 @@ import (
 	"context"
 	"crypto/ecdsa"
 	"fmt"
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
-	"log"
 	"math/big"
-	"os"
 	"strconv"
+	"strings"
 	"time"
 )
 
-func getNonce(node, priv string) (uint64, error) {
+func getNonce(node, priv string, output *widget.Entry) (uint64, error) {
 	privateKey, err := crypto.HexToECDSA(priv)
 	if err != nil {
-		log.Fatalf("Failed to convert private key: %v", err)
+		appendOutput(output, fmt.Sprintf("Failed to convert private key: %v\n", err))
 		return 0, err
 	}
 
 	publicKey := privateKey.Public()
 	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
 	if !ok {
-		log.Fatal("Error casting public key to ECDSA")
+		appendOutput(output, "Error casting public key to ECDSA\n")
 		return 0, err
 	}
 
 	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
 	client, err := ethclient.Dial(node)
 	if err != nil {
-		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
+		appendOutput(output, fmt.Sprintf("Failed to connect to the Ethereum client: %v\n", err))
 		return 0, err
 	}
 	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
 	if err != nil {
-		log.Fatalf("Failed to get nonce: %v", err)
+		appendOutput(output, fmt.Sprintf("Failed to get nonce: %v\n", err))
 		return 0, err
 	}
-	fmt.Println("nonce now is ------- ", nonce)
+	appendOutput(output, fmt.Sprintf("Nonce obtained: %d\n", nonce))
 	return nonce, nil
 }
-func getChainID(node string) (*big.Int, error) {
+
+func getChainID(node string, output *widget.Entry) (*big.Int, error) {
 	client, err := ethclient.Dial(node)
 	if err != nil {
-		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
+		appendOutput(output, fmt.Sprintf("Failed to connect to the Ethereum client: %v\n", err))
 		return nil, err
 	}
 	chainID, err := client.NetworkID(context.Background())
 	if err != nil {
-		log.Fatalf("Failed to get chain ID: %v", err)
+		appendOutput(output, fmt.Sprintf("Failed to get chain ID: %v\n", err))
 		return nil, err
 	}
-	fmt.Println("chain id is ------- ", chainID)
+	appendOutput(output, fmt.Sprintf("Chain ID obtained: %s\n", chainID.String()))
 	return chainID, nil
 }
-func do(node string, priv string, msg string, nonce uint64, chainID *big.Int, gasPrice_ int64) error {
+
+func do(node, priv, msg string, nonce uint64, chainID *big.Int, gasPrice_ int64, output *widget.Entry) error {
 	client, err := ethclient.Dial(node)
 	if err != nil {
-		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
+		appendOutput(output, fmt.Sprintf("Failed to connect to the Ethereum client: %v\n", err))
 		return err
 	}
 
 	privateKey, err := crypto.HexToECDSA(priv)
 	if err != nil {
-		log.Fatalf("Failed to convert private key: %v", err)
+		appendOutput(output, fmt.Sprintf("Failed to convert private key: %v\n", err))
 		return err
 	}
 
 	publicKey := privateKey.Public()
 	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
 	if !ok {
-		log.Fatal("Error casting public key to ECDSA")
+		appendOutput(output, "Error casting public key to ECDSA\n")
 	}
 
 	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
-
 	value := big.NewInt(0)
 	gasLimit := uint64(22000)
 	gasPrice := big.NewInt(gasPrice_)
-	if err != nil {
-		log.Fatalf("Failed to suggest gas price: %v", err)
-		return err
-	}
+
 	data := []byte(msg)
 	tx := types.NewTransaction(nonce, fromAddress, value, gasLimit, gasPrice, data)
 	signedTx, err := types.SignTx(tx, types.NewEIP155Signer(chainID), privateKey)
 	if err != nil {
-		log.Fatalf("Failed to sign transaction: %v", err)
+		appendOutput(output, fmt.Sprintf("Failed to sign transaction: %v\n", err))
 		return err
 	}
 	err = client.SendTransaction(context.Background(), signedTx)
 	if err != nil {
-		log.Fatalf("Failed to send transaction: %v", err)
+		appendOutput(output, fmt.Sprintf("Failed to send transaction: %v\n", err))
 		return err
 	}
 
-	fmt.Printf("Transaction sent! TX Hash: %s\n", signedTx.Hash().Hex())
+	appendOutput(output, fmt.Sprintf("Transaction sent! TX Hash: %s\n", signedTx.Hash().Hex()))
 	return nil
 }
-func main() {
-	var (
-		Node      = os.Args[1]
-		Priv      = os.Args[2]
-		Msg       = os.Args[3]
-		Gas       = os.Args[4]
-		LoopTotal = os.Args[5]
-		SleepTime = os.Args[6]
-	)
 
-	nonceNow, err := getNonce(Node, Priv)
+func startSending(node, priv, msg string, gas, loopTotal, sleepTime int64, output *widget.Entry) error {
+	nonceNow, err := getNonce(node, priv, output)
 	if err != nil {
-		panic(err)
+		return err
 	}
 
-	chainID, err := getChainID(Node)
+	chainID, err := getChainID(node, output)
 	if err != nil {
-		panic(err)
+		return err
 	}
 
-	gas, err := strconv.ParseInt(Gas, 10, 64)
-	if err != nil {
-		panic(err)
-	}
-
-	loopTotal_, err := strconv.ParseInt(LoopTotal, 10, 64)
-	if err != nil {
-		panic(err)
-	}
-
-	sleep_, err := strconv.ParseInt(SleepTime, 10, 64)
-	if err != nil {
-		panic(err)
-	}
-	for i := 0; i < int(loopTotal_); i++ {
-		time.Sleep(time.Duration(sleep_) * time.Millisecond)
-		if err := do(Node, Priv, Msg, nonceNow, chainID, gas); err != nil {
-			panic(err)
+	for i := 0; i < int(loopTotal); i++ {
+		time.Sleep(time.Duration(sleepTime) * time.Millisecond)
+		if err := do(node, priv, msg, nonceNow, chainID, gas, output); err != nil {
+			return err
 		}
 		nonceNow++
 	}
+
+	appendOutput(output, "All transactions sent successfully!\n")
+	return nil
+}
+
+func appendOutput(output *widget.Entry, text string) {
+	lines := strings.Split(output.Text, "\n")
+	if len(lines) > 500 {
+		lines = lines[len(lines)-500:] // Keep only the last 500 lines
+	}
+	lines = append(lines, text)
+	output.SetText(strings.Join(lines, "\n"))
+	output.Refresh() // Refresh the widget to update the text
+}
+
+func main() {
+	a := app.New()
+	w := a.NewWindow("Ethereum Transaction Sender")
+	nodeEntry := widget.NewEntry()
+	nodeEntry.SetPlaceHolder("https://rpc.ankr.com/bsc/xxxxxxxx")
+
+	privEntry := widget.NewPasswordEntry()
+	privEntry.SetPlaceHolder("your priv")
+
+	msgEntry := widget.NewEntry()
+	msgEntry.SetPlaceHolder(`data:,{"p":"bsc-20","op":"mint","tick":"bsci","amt":"1000"}`)
+
+	gasEntry := widget.NewEntry()
+	gasEntry.SetPlaceHolder("6000000000")
+
+	loopTotalEntry := widget.NewEntry()
+	loopTotalEntry.SetPlaceHolder("10")
+
+	sleepTimeEntry := widget.NewEntry()
+	sleepTimeEntry.SetPlaceHolder("100")
+	outputEntry := widget.NewMultiLineEntry()
+	outputEntry.Wrapping = fyne.TextWrapWord
+
+	scrollContainer := container.NewVScroll(outputEntry)
+	scrollContainer.SetMinSize(fyne.NewSize(800, 300))
+
+	startButton := widget.NewButton("Start", func() {
+		node := nodeEntry.Text
+		priv := privEntry.Text
+		msg := msgEntry.Text
+		gas, err := strconv.ParseInt(gasEntry.Text, 10, 64)
+		if err != nil {
+			appendOutput(outputEntry, fmt.Sprintf("Invalid gas price: %v\n", err))
+			return
+		}
+		loopTotal, err := strconv.ParseInt(loopTotalEntry.Text, 10, 64)
+		if err != nil {
+			appendOutput(outputEntry, fmt.Sprintf("Invalid loop total: %v\n", err))
+			return
+		}
+		sleepTime, err := strconv.ParseInt(sleepTimeEntry.Text, 10, 64)
+		if err != nil {
+			appendOutput(outputEntry, fmt.Sprintf("Invalid sleep time: %v\n", err))
+			return
+		}
+
+		if err := startSending(node, priv, msg, gas, loopTotal, sleepTime, outputEntry); err != nil {
+			appendOutput(outputEntry, fmt.Sprintf("Error: %s", err.Error()))
+		}
+	})
+
+	w.SetContent(container.NewVBox(
+		widget.NewLabel("Node URL"),
+		nodeEntry,
+		widget.NewLabel("Private Key"),
+		privEntry,
+		widget.NewLabel("Message"),
+		msgEntry,
+		widget.NewLabel("Gas Price"),
+		gasEntry,
+		widget.NewLabel("Loop Total"),
+		loopTotalEntry,
+		widget.NewLabel("Sleep Time (ms)"),
+		sleepTimeEntry,
+		startButton,
+		scrollContainer,
+	))
+
+	w.ShowAndRun()
 }
--- readme.md
+++ readme.md
@@ -1,23 +1,16 @@
 ### 在各种 evm 链上打铭文的脚本工具
-#### 让不会写代码的人也能批量打铭文, 再一次站在同一起跑线上.
-使用以下命令格式来执行二进制文件：
 
+### 让不会写代码的人也能批量打铭文, 再一次站在同一起跑线上.
 
-```shell
-./对应的二进制文件 "换成你的节点" "换成你的私钥" '铭文的格式' "换成你要改的 gas,单位是 wei" "换成你要打的总量" "换成休息的间隔,单位是毫秒"
-```
+实际例子
+https://bscscan.com/tx/0x4144f201eb229640e1dae6f7c762252b5d7482d010f68fd8922a87f58ec419d9
+https://goerli.etherscan.io/tx/0x9c7bd9f6660e80f0e9d725571fb4b58886c9ae679cf156e6b8612b1cc8fafc01
 
 #### 使用步骤
 
 1. clone 或者 下载 [本仓库](https://github.com/0xJayShen/EVM-BRC20-BATCH/archive/refs/heads/main.zip)
 2. 使用对应的二进制文件运行
-   - mac 用户
-    ```shell
-    ./mac https://rpc.ankr.com/bsc/2eac989010412c2411daf2a 64431432344y4460dsfsdaf2d6c2f 'data:,{"p":"bsc-20","op":"mint","tick":"bsci","amt":"1000"}' 6000000000 1 1
-    
-    ```
-   - windows 用户(待测试)
-   ```shell
-   ./windows.exe https://rpc.ankr.com/bsc/2eac989010412c2411daf2a 64431432344y4460dsfsdaf2d6c2f 'data:,{"p":"bsc-20","op":"mint","tick":"bsci","amt":"1000"}' 6000000000 1 1
-
-    ```
+    - mac 用户
+      双击 mac 文件
+    - windows 用户(待测试)
+      双击 windows.exe 文件
